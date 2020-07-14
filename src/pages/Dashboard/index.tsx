a46dd0fc4dcbf287b07d325f43cc07bf4ffa59ab
Bruno Moura <bruno8moura@gmail.com>
2020-07-14 06:44:42 -0300

    Handle errors.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-import { Title, Form, Repositories } from './styles';
+import {
+  Title, Form, Repositories, Error,
+} from './styles';
 import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
@@ -18,15 +20,26 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [searchedRepo, setSearchedRepo] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
 
-    if (!searchedRepo) return;
-    const response = await api.get<Repository>(`repos/${searchedRepo.trim()}`);
-    const repository: Repository = response.data;
-    setRepositories([...repositories, repository]);
-    setSearchedRepo('');
+    if (!searchedRepo) {
+      setErrorMessage('Informar um repositório válido');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${searchedRepo.trim()}`);
+      const repository: Repository = response.data;
+      setRepositories([...repositories, repository]);
+      setSearchedRepo('');
+      setErrorMessage('');
+    } catch (error) {
+      console.log(error);
+      setErrorMessage('Repositório não encontrado.');
+    }
   }
 
   return (
@@ -34,14 +47,20 @@ const Dashboard: React.FC = () => {
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form onSubmit={handleAddRepository}>
-        <input placeholder="Digite o nome do reposositório" value={searchedRepo} onChange={(e) => setSearchedRepo(e.target.value)} />
+      <Form hasError={!!errorMessage} onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do reposositório"
+          value={searchedRepo}
+          onChange={(e) => setSearchedRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      { errorMessage && <Error>{errorMessage}</Error>}
+
       <Repositories>
         {repositories.map((repository) => (
-          <a href="test">
+          <a href="test" key={repository.full_name}>
             <span>
               <img
                 src={repository.owner.avatar_url}
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { shade } from 'polished';
+
+interface FormProps {
+    hasError: boolean;
+}
 
 export const Title = styled.h1`
     font-size: 48px;
@@ -10,11 +14,12 @@ export const Title = styled.h1`
     line-height: 56px
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
     margin-top: 40px;
     max-width: 700px;
 
     display: flex;
+
 
     input {
         flex: 1;
@@ -22,10 +27,17 @@ export const Form = styled.form`
         padding: 0 24px;
         border: 0;
         border-radius: 5px 0 0 5px;
+        color: #3a3a3a;
+        border: 2px solid #FFF;
+        border-right: 0;
 
         &::placeholder {
             color: #a8a8b3;
         }
+
+        ${(props) => props.hasError && css`
+            border-color: #c53030;
+        `}
     }
 
     button {
@@ -99,4 +111,10 @@ export const Repositories = styled.div`
     .arrow {
         color: #CBCBD6;
     }
+`;
+
+export const Error = styled.span`
+    display: block;
+    color: #c53030;
+    margin-top: 8px;
 `;
